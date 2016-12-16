b7fcdc67e65b3bca8e926e7d34d708699a5befd2
Andrew Accuardi <AndrewAccuardi@gmail.com>
2016-12-15 21:01:59 -0800

    Add passing spec testing relationship between stores and brands from the store end

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,17 @@ ENV['RACK_ENV'] = 'test'
 require('rspec')
 require('pg')
 require('sinatra/activerecord')
-# require('project')
+require('store')
+require('brand')
+require('shoulda-matchers')
 
 RSpec.configure do |config|
   config.after(:each) do
-    # Project.all.each(&:destroy)
+    Store.all().each() do |store|
+  store.destroy()
+end
+Brand.all.each do |brand|
+  brand.destroy
+end
   end
 end
