5ff0265ecba73c794d05ea62e8125e6efe8ef32a
Andrew Accuardi <andrewaccuardi@gmail.com>
2016-12-16 15:11:06 -0800

    Creates fully functional front-end for shoestore application.

--- app.rb
+++ app.rb
@@ -3,8 +3,73 @@ require("sinatra/reloader")
 require('sinatra/activerecord')
 also_reload("lib/**/*.rb")
 require("pg")
-# require('./lib/project')
+require('pry')
+require('./lib/brand')
+require('./lib/store')
 
 get('/') do
+  @stores = Store.all
+  @brands = Brand.all
   erb(:index)
+end
+
+# STORE
+
+get('/stores/:id') do
+  @store = Store.find(params[:id].to_i)
+  @brands = Brand.all
+  erb :store
+end
+
+post('/stores') do
+  name = params[:store_name]
+  Store.create({name: name})
+  redirect('/')
+end
+
+get('/stores/:id/edit') do
+  @store = Store.find(params[:id].to_i)
+  erb :stores_edit
+end
+
+patch('/stores/:id') do
+  name = params.fetch("store_update")
+  @store = Store.find(params[:id].to_i)
+  @store.update({:name => name})
+  erb :store
+end
+
+delete('/stores/:id') do
+  @store = Store.find(params[:id].to_i)
+  @store.delete
+  @stores = Store.all
+  redirect('/')
+end
+
+#BRAND
+
+post('/brands') do
+  name = params[:brand_name]
+  Brand.create({name: name})
+  @brands = Brand.all
+  redirect('/')
+end
+
+post('/stores/:id/') do
+  name = params[:brand]
+  @brands = Brand.all
+  @store = Store.find(params[:store_id].to_i)
+  new_brand = Brand.where({name: name})
+  @store.brands.push(new_brand)
+  erb :store
+end
+
+get("/method") do
+  Store.all.each do |store|
+    store.destroy
+  end
+  Brand.all.each do |brand|
+    brand.destroy
+  end
+  redirect "/"
 end
