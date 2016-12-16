957f4b00f1aced38bb9af5e3e13ab84fb46621dc
Andrew Accuardi <AndrewAccuardi@gmail.com>
2016-12-15 22:56:26 -0800

    Add passing spec to use callback to save the store name as capitalized.

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -6,4 +6,8 @@ describe Store do
     store = Store.new(name: "")
     expect(store.save).to eq false
   end
+  it "capitalizes the name the user inputs" do
+    store = Store.create({name: "foot locker"})
+    expect(store.name).to eq "Foot Locker"
+  end
 end
